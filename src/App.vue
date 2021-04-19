7808c97c569058df923973202bc949d4a22dff71
asadbr <barotov.asadbek@gmail.com>
2021-04-19 11:08:24 +0500

    [90 of project is done]

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,10 @@
           :done="item.done"
           :id="item.id"
           @checkbox-changed="updateDoneStatus(item.id)"
-        />
+          @item-deleted="deleteToDo(item.id)"
+          @item-edited="editToDo(item.id, $event)"
+        >
+        </to-do-item>
       </li>
     </ul>
   </div>
@@ -48,6 +51,14 @@ export default {
     updateDoneStatus(toDoId) {
       const toDoToUpdate = this.ToDoItems.find((item) => item.id === toDoId);
       toDoToUpdate.done = !toDoToUpdate.done;
+    },
+    deleteToDo(toDoId) {
+      const itemIndex = this.ToDoItems.findIndex((item) => item.id === toDoId);
+      this.ToDoItems.splice(itemIndex, 1);
+    },
+    editToDo(toDoId, newLabel) {
+      const toDoToEdit = this.ToDoItems.find((item) => item.id === toDoId);
+      toDoToEdit.label = newLabel;
     },
   },
   computed: {
